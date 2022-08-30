7816ae689a0cae5b74732607e2b4dd2c25d94aaa
HayeonP <kite9240@gmail.com>
2022-08-30 16:32:01 +0900

    cross build is removed

--- cross_build/install_kernel.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-KERNEL_TOP_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
-MOD_OUTPUT="$KERNEL_TOP_PATH/modules"
-OUTPUT="$KERNEL_TOP_PATH/out"
-
-cd linux
-
-sudo make O=$OUTPUT ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- modules_install INSTALL_MOD_PATH=$MOD_OUTPUT
